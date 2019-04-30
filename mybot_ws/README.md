a17a4072866e6454cf73acffb652089dbceca951
HarunTeper <hteper@web.de>
2019-04-30 14:57:21 +0200

    initial commit

---
+++ mybot_ws/README.md
@@ -0,0 +1,3 @@
+# mybot_ws
+URDF model for Gazebo integrated with ROS
+# test_teb_local_planner_implementation
---
+++ mybot_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/harun/catkin_ws/src/mybot_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/harun/catkin_ws/devel;/home/harun/autoracing-etitar18ws/devel;/opt/ros/kinetic".split(';') if "/home/harun/catkin_ws/devel;/home/harun/autoracing-etitar18ws/devel;/opt/ros/kinetic" != "" else []
